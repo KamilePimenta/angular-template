f9297c3228cbdde6b9c358c04b8e51711ed702ef
Kamile Pimenta <kamile.pimenta@outlook.com>
2023-04-20 02:26:24 -0300

    fix: updated husky and eslint config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,9 @@ import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
+
 	beforeEach(async () => {
+
 		await TestBed.configureTestingModule({
 			imports: [
 				RouterTestingModule
@@ -12,24 +14,37 @@ describe('AppComponent', () => {
 				AppComponent
 			],
 		}).compileComponents()
+
 	})
 
 	it('should create the app', () => {
+
 		const fixture = TestBed.createComponent(AppComponent)
 		const app = fixture.componentInstance
+
 		expect(app).toBeTruthy()
+
 	})
 
 	it('should have as title \'angular-full\'', () => {
+
 		const fixture = TestBed.createComponent(AppComponent)
 		const app = fixture.componentInstance
+
 		expect(app.title).toEqual('angular-full')
+
 	})
 
 	it('should render title', () => {
+
 		const fixture = TestBed.createComponent(AppComponent)
+
 		fixture.detectChanges()
 		const compiled = fixture.nativeElement as HTMLElement
-		expect(compiled.querySelector('.content span')?.textContent).toContain('angular-full app is running!')
+
+		expect(compiled.querySelector('.content span')?.textContent)
+			.toContain('angular-full app is running!')
+
 	})
+
 })
